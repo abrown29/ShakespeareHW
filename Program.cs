b7d81f23de3cf904d66f04e458fbbdb5e91285c0
Amanda Brown <amanda.brown@gmail.com>
2016-09-22 12:00:12 -0400

    shakespeare homework done

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ShakespeareHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("..\\..\\Shakespeare.txt");
+
+            string line = reader.ReadLine();
+            Console.WriteLine(line);
+
+            reader.Close();
+
+            StreamReader newReader = new StreamReader("..\\..\\Shakespeare.txt");
+
+            string entire = newReader.ReadToEnd();
+            Console.WriteLine(entire);
+            newReader.Close();
+
+            StreamReader brandNewReader = new StreamReader("..\\..\\Shakespeare.txt");
+            string toCapital = brandNewReader.ReadToEnd();
+            string result = toCapital.Replace('a', 'A');
+            result = result.Replace('e', 'E');
+            result = result.Replace('i', 'I');
+            result = result.Replace('o', 'O');
+            result = result.Replace('u', 'U');
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
